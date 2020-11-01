064a9bdd1c20f4610bc8286846a7d1285e0e0109
Bocmen <Bocmen@users.noreply.github.com>
2020-11-01 21:04:25 +0300

    Улучшена загрузка шейерной программы с шейдерами

--- Project2/Project2/Shader.h
+++ Project2/Project2/Shader.h
@@ -8,23 +8,26 @@
 #define TextLogErrorCompileShader "Ошибка компиляции шейдера, расположеного по пути"
 #define TextErrorCompileProgramShaders "Не удалось скомпилировать программу шейдера"
 #define TextCompileShadersError "Произошла ошибка компиляции одного из шейдеров"
+#define TextExeptionError "Возникла ошибка код ошибки:"
 
 class Shader
 {
 public:
 	uint32_t ShaderId_GL;
+	bool StateBild = false;
 
 	Shader(const char* VertexPath, const char* FragmentPath)
 	{
 		uint32_t shaderVertex = glCreateShader(GL_VERTEX_SHADER);
 		uint32_t shaderFragment = glCreateShader(GL_FRAGMENT_SHADER);
-		if (!(shaderLoad(VertexPath, shaderVertex) && shaderLoad(FragmentPath, shaderFragment)))
+		StateBild = (shaderLoad(VertexPath, shaderVertex) && shaderLoad(FragmentPath, shaderFragment));
+		if (!StateBild)
 			std::cout << TextCompileShadersError << std::endl;
 		ShaderId_GL = glCreateProgram();
 		glAttachShader(ShaderId_GL, shaderVertex);
 		glAttachShader(ShaderId_GL, shaderFragment);
 		glLinkProgram(ShaderId_GL);
-		chackCompile(ShaderId_GL, ProgramType, NULL);
+		StateBild = chackCompile(ShaderId_GL, ProgramType, NULL) && StateBild;
 		glDeleteShader(shaderVertex);
 		glDeleteShader(shaderFragment);
 	}
@@ -35,6 +38,13 @@ private:
 		ShaaderType,
 		ProgramType
 	};
+	/// <summary>
+	/// Проверка успешности компиляции
+	/// </summary>
+	/// <param name="id">Индификатор обьекта (шейдер или программа)</param>
+	/// <param name="type">Тип обьекта</param>
+	/// <param name="filePatch">Доп параметр для шейдеров</param>
+	/// <returns>успешность компиляции</returns>
 	bool chackCompile(uint32_t id, typeCompileCheck type, const char* filePatch)
 	{
 		int idEror;
@@ -59,31 +69,35 @@ private:
 		return true;
 	}
 	/// <summary>
-	/// Чтение текстового файла целиком
+	/// Загрузка шейдера
 	/// </summary>
-	/// <param name="FilePatch">Путь к файлу</param>
-	/// <returns></returns>
-	const char** FileRead(const char* FilePatch)
-	{
-		std::ifstream is(FilePatch);
-		std::vector<std::string> resul;
-		std::string strBuff;
-		const char* charsbuff;
-		while (!is.eof())
-		{
-			std::getline(is, strBuff);
-			resul.push_back(strBuff);
-		}
-		const char* res = resul[0].c_str();
-		return &res;
-	}
+	/// <param name="filePatch">Путь к шейдеру</param>
+	/// <param name="shaderId">Индификатор СОЗДАННОГО шейдера</param>
+	/// <returns>результат успешности компиляции</returns>
 	bool shaderLoad(const char* filePatch, uint32_t& shaderId)
 	{
-		const char** ReadShader = FileRead(filePatch);
-		//std::string textShader = FileRead(filePatch);
-		//const char* textShaderChar_C = textShader.c_str();
-		glShaderSource(shaderId, 6, ReadShader, NULL);
-		glCompileShader(shaderId);
-		return chackCompile(shaderId, ShaaderType, filePatch);
+		try
+		{
+			std::ifstream is(filePatch);
+			std::vector<const char*> lines;
+			std::vector<int32_t> lengths;
+			std::string* strBuff;
+			while (!is.eof())
+			{
+				strBuff = new std::string;
+				std::getline(is, *strBuff);
+				strBuff->append("\n");
+				lengths.push_back(strBuff->size());
+				lines.push_back((*strBuff).data());
+			}
+			glShaderSource(shaderId, lines.size(), lines.data(), lengths.data());
+			glCompileShader(shaderId);
+			return chackCompile(shaderId, ShaaderType, filePatch);
+		}
+		catch (int CodeError)
+		{
+			std::cout << TextExeptionError << CodeError << std::endl;
+			return false;
+		}
 	}
 };
